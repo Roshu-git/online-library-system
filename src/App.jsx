6b99bd6ab03adade5600291c29a6a7b7db51ff06
Roshu-git <roshaniadlak15@gmail.com>
2026-08-09 10:26:24 +0530

    show books in the home page is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import BrowseBooks from './pages/BrowseBooks';
+import BookDetails from './pages/BookDetails';
+import AddBook from './pages/AddBook';
 
-function App() {
+export default function App() {
   return (
-    <div>App</div>
-  )
-}
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books/:category" element={<BrowseBooks />} />
+        <Route path="/book/:id" element={<BookDetails />} />
+        <Route path="/add-book" element={<AddBook />} />
+      </Routes>
+    </div>
+  );
+}
